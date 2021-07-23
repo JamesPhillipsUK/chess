8bc445f72a739642b0cdf57a3c8b85b9c4682235
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2021-07-24 00:21:12 +0100

    Added a grid of squares to the board.

--- chess.py
+++ chess.py
@@ -1,10 +1,14 @@
 import PySimpleGUI as psg
 from board import *
+from square import *
 
 class Chess:
   def main():
     board = Board()
-    board.buildBoard(psg.Window(title="Chess", layout=[[]], margins=(296, 296)))
+    layout = []
+    for cnt in range(8, 0, -1):
+      layout += [[Square("a" + str(cnt))] + [Square("b" + str(cnt))] + [Square("c" + str(cnt))] + [Square("d" + str(cnt))] + [Square("e" + str(cnt))] + [Square("f" + str(cnt))] + [Square("g" + str(cnt))] + [Square("h" + str(cnt))]]
+    board.buildBoard(psg.Window(title="Chess", layout=layout, margins=(296, 296)))
     board.launchGameLoop()
     print("ran successfully!")
     
