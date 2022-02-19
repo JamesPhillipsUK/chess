dbb7047cce5692869b234a5e5695837d0cfa4e2d
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-02-19 18:33:35 +0000

    improved algorithm for setting the initial board layout.

--- board.py
+++ board.py
@@ -7,28 +7,14 @@ class Board:
     window = psg.Window(title="Chess")
     whiteSquare = b''
     blackSquare = b''
-    layout = []
+    layout = [[], [], [], [], [], [], [], []]
 
     def setInitialLayout(self):
-        self.layout = []
         for cnt in range(8, 0, -1):
-            self.layout += [
-                       [Square("a" + str(cnt),
-                        image_data=self.getSquareColour("a" + str(cnt)))] +
-                       [Square("b" + str(cnt),
-                        image_data=self.getSquareColour("b" + str(cnt)))] +
-                       [Square("c" + str(cnt),
-                        image_data=self.getSquareColour("c" + str(cnt)))] +
-                       [Square("d" + str(cnt),
-                        image_data=self.getSquareColour("d" + str(cnt)))] +
-                       [Square("e" + str(cnt),
-                        image_data=self.getSquareColour("e" + str(cnt)))] +
-                       [Square("f" + str(cnt),
-                        image_data=self.getSquareColour("f" + str(cnt)))] +
-                       [Square("g" + str(cnt),
-                        image_data=self.getSquareColour("g" + str(cnt)))] +
-                       [Square("h" + str(cnt),
-                        image_data=self.getSquareColour("h" + str(cnt)))]]
+            for col in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+                self.layout[cnt-1] += [Square(key=col + str(cnt),
+                                              image_data=self.getSquareColour(
+                                                  col + str(cnt)))]
 
     def getLayout(self):
         return self.layout
@@ -40,8 +26,8 @@ class Board:
                 return self.whiteSquare
         else:
             if int(squareNameSpec[1]) % 2 != 0:
-                return  self.whiteSquare
-        return  self.blackSquare
+                return self.whiteSquare
+        return self.blackSquare
 
     def convertImageToB64(self, filename: str):
         with open(filename, 'rb') as imageFile:
