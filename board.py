bb4f065e07c806ce423f915c4f6a226b0a2fe005
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-02-19 16:44:55 +0000

    Added methods for loading image assets as b64 for use on the board.

--- board.py
+++ board.py
@@ -1,8 +1,20 @@
 import PySimpleGUI as psg
+import base64
 
 
 class Board:
     window = psg.Window(title="Chess")
+    whiteSquare = b''
+    blackSquare = b''
+
+    def convertImageToB64(self, filename: str):
+        with open(filename, 'rb') as imageFile:
+            image = imageFile.read()
+            return base64.b64encode(image)
+
+    def loadAssets(self):
+        self.whiteSquare = self.convertImageToB64("img/w_square.png")
+        self.blackSquare = self.convertImageToB64("img/b_square.png")
 
     def buildBoard(self, win):
         self.window = win
