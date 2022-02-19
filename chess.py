37b07e1e0164e04a5e03545a7b589c155935a9f7
Jesse Phillips <JamesPhillipsUK@users.noreply.github.com>
2022-02-19 17:03:03 +0000

    Chess now runs an instance of itself, which lays out the board correctly.

--- chess.py
+++ chess.py
@@ -4,24 +4,37 @@ from square import *
 
 
 class Chess:
-    def main():
+    def __init__(self):
+        pass
+
+    def main(self):
         board = Board()
+        board.loadAssets()
         layout = []
         for cnt in range(8, 0, -1):
             layout += [
-                       [Square("a" + str(cnt))] +
-                       [Square("b" + str(cnt))] +
-                       [Square("c" + str(cnt))] +
-                       [Square("d" + str(cnt))] +
-                       [Square("e" + str(cnt))] +
-                       [Square("f" + str(cnt))] +
-                       [Square("g" + str(cnt))] +
-                       [Square("h" + str(cnt))]]
+                       [Square("a" + str(cnt),
+                        image_data=board.getSquareColour("a" + str(cnt)))] +
+                       [Square("b" + str(cnt),
+                        image_data=board.getSquareColour("b" + str(cnt)))] +
+                       [Square("c" + str(cnt),
+                        image_data=board.getSquareColour("c" + str(cnt)))] +
+                       [Square("d" + str(cnt),
+                        image_data=board.getSquareColour("d" + str(cnt)))] +
+                       [Square("e" + str(cnt),
+                        image_data=board.getSquareColour("e" + str(cnt)))] +
+                       [Square("f" + str(cnt),
+                        image_data=board.getSquareColour("f" + str(cnt)))] +
+                       [Square("g" + str(cnt),
+                        image_data=board.getSquareColour("g" + str(cnt)))] +
+                       [Square("h" + str(cnt),
+                        image_data=board.getSquareColour("h" + str(cnt)))]]
         board.buildBoard(psg.Window(title="Chess",
                                     layout=layout,
-                                    margins=(296, 296)))
+                                    margins=(0, 0)))
         board.launchGameLoop()
         print("ran successfully!")
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    chess = Chess()
+    chess.main()
